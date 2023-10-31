bee3fa052e64bbb348e09ad43a8ce09738d8ce09
luisadha <luisadha@noreply.codeberg.org>
2023-10-31 19:43:34 +0000

    Upload files to "/"
    Signed-off-by: luisadha <luisadha@noreply.codeberg.org>

---
+++ drawercli.sh
@@ -0,0 +1,62 @@
+
+source $HOME/storage/shared/termuxlauncher/.apps-launcher # inisialisasi termuxlauncher, ini akan membaca seluruh aplikasi pengguna yang diinstall di perangkat
+
+
+
+function backKey() {
+
+input keyevent 4
+}
+
+backKey
+
+opt="$1"
+if [ -z "$opt" ]; then
+
+
+
+launch $(launch -l | grep -Exo '[a-z0-9:_-]+' | sort -u | xargs | lolcat -r | xargs -n 1 | pick) >> $HOME/.drawercli_history
+
+exit 0;
+
+elif [ "$opt" == "--skip" ]; then # THIS OPTION SKIP
+
+launch $(launch -l | grep -o "termux" )
+
+exit 0;
+
+elif [ "$opt" == "-S" ]; then # THIS OPTION SUGGESTIONS
+
+ERROR=${2?Error: option requires an argument -- r }
+
+
+      launch $(launch -l | grep -Exo [a-z0-9:_-]+ | sort -u | xargs | lolcat -r | xargs -n 1 | shuf | head -n "$2" | pick)
+
+   backKey
+
+    echo $ERROR
+
+ exit 0;
+
+elif [ "$opt" == "-u" ]; then # THIS OPTION MOST USAGE
+
+  launch $(launch -l | grep -Exo [a-z0-9:_-]+ | cat $HOME/.drawercli_history | uniq -c | sort -r | grep -oP "(?<=')[^'\t0-9]*(?=')" | head -n 1 | pick )
+
+  exit 0;
+
+elif [ "$opt" == "-r" ]; then # THIS OPTION FOR REFRESH LIST APPS BY termuxlauncher
+
+
+launch $(launch -l | grep "termuxlauncher" )
+
+exit 0;
+
+ else
+
+
+       echo "$0: options not recognized! or require more arguments after -$opt";
+     exit 1;
+
+fi
+
+

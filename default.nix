f5ec3bc2e88f9532def000414cb6c996eb6fdd70
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-09-09 22:02:26 +0700

    Create default.nix

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "drawercli";
+  version = "1.2.3";
+
+  src = ./.;
+
+  buildInputs = [ pkgs.bash ]; 
+
+  buildPhase = ''
+    mkdir -p $out/bin
+    cp -r * $out/bin
+  '';
+
+  installPhase = ''
+    
+    chmod +x $out/bin/drawercli  
+  '';
+
+  meta = with pkgs.lib; {
+    description = "This cli app supports using termuxlauncher as your primary launcher for Android phones, interactively and easily install. That's simply the Termuxlauncher Add-on built with fzf🚀
+";
+    license = licenses.mit; 
+    maintainers = with maintainers; [ luisadha ]; 
+  };
+}
